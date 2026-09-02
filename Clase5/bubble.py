efa98c9d1a8d3ee273f2e9c96c1c05a31640bd04
SalvadorRGDev <salvadorrg.dev@gmail.com>
2026-09-02 08:28:28 -0600

    Update bubble.py

--- Clase5/bubble.py
+++ Clase5/bubble.py
@@ -1,1 +1,10 @@
-
+def bubble_sort_brute_force(arr):
+    n = len(arr)
+    # Ciclo externo corre n veces de forma fija
+    for i in range(n):
+        # Ciclo interno compara elementos adyacentes
+        for j in range(0, n - 1):
+            if arr[j] > arr[j + 1]:
+                # Intercambio de elementos
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
